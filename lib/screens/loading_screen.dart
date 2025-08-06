5b345ddf5b55b051b87c1deeccb4c7c3d8a76fb7
Munyaradzi Tamayi <munyaradzitamayii@gmail.com>
2025-08-06 16:26:50 +0200

    implement the openWeatherApi
    get weather details using the open weather API

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
